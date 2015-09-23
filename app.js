e18be099fdebbcc9afc3ef0edbb2b002f62a7399
tesco <tesco@tescos-MacBook-Pro-6.local>
2015-09-24 03:49:12 +0530

    heroku

--- app.js
+++ app.js
@@ -65,8 +65,8 @@ app.use(function(err, req, res, next) {
 });
 
 
-// module.exports = app;
-app.listen('3000',function(){
-  console.log('listening on port 3000');
-})
+module.exports = app;
+// app.listen('3000',function(){
+//   console.log('listening on port 3000');
+// })
 
